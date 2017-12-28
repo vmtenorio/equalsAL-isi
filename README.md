dc744ba7f338efdf95d466d1e4996983459e8cfe
Victor Manuel Tenorio <bossipo@debian-Bossipo.portatil-Bossipo.com>
2017-12-28 01:09:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ejercicio equals de AbstractList de Java
+Hecho en un fichero de JUnit para poder ir probando los test que vaya realizando.
+
---
+++ src/equalsALTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.*;
+
+public class equalsALTest {
+	
+	List<String> list1;
+	List<String> list2;
+
+	@Before
+	public void setUp() throws Exception {
+		list1 = new ArrayList<String>();
+		list2 = new ArrayList<String>();
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
